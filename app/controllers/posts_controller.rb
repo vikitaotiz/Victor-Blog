39d12c0e390e39b902d8ad33026679595566d920
Victor Otieno <victorotieno598@gmail.com>
2021-06-02 22:36:05 +0300

    Add show,new,create method in posts controler

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,31 @@
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to @post, notice: "Post was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
 end
