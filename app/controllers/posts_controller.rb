eb175f0e0596a4b570a2255c8be0911f3e385441
Victor Otieno <victorotieno598@gmail.com>
2021-06-02 22:47:34 +0300

    Add edit and update methods in posts controler

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,20 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
   private def post_params
     params.require(:post).permit(:title, :body)
   end
